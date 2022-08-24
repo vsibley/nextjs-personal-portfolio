5e674014d4f2ba0552dae7c21aa1e0b14b4541cd
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-08-24 15:50:26 -0700

    updated contact and projects components w/dark UI

--- public/components/Projects.jsx
+++ public/components/Projects.jsx
@@ -6,15 +6,15 @@ import portfolio from '../assets/projects/portfolio.png';
 
 const Projects = () => {
     return (
-        <div id='projects' className='w-full h-full px-4'>
+        <div id='projects' className='w-full h-full px-4 dark:bg-black dark:text-white'>
             <div className='max-w-[1240px] mx-auto px-2 w-full max-h-full py-16'>
 
-                <p className='text-xl tracking-wider uppercase text-[#FF5733] dark:text-[#395B64] py-4'>Projects</p>
+                <p className='text-xl tracking-wider uppercase text-[#FF5733] dark:text-[#395B64]  py-4'>Projects</p>
                 <h2 className=' pb-6'>My most recent work</h2>
-                <div className='grid md:grid-cols-2 gap-8'>
+                <div className='grid md:grid-cols-2 gap-8 '>
 
-                    <div className='shadow-2xl rounded-xl'>
-                        <div className='grid grid-cols-1 gap-4 justify-center items-center text-center'>
+                    <div className='shadow-2xl rounded-xl dark:shadow-gray-800'>
+                        <div className='grid grid-cols-1 gap-4 justify-center items-center text-center '>
                             <Image className='rounded-xl' src={techblog} alt='' />
                             <div className='p-3'>
                                 <p className='py-2'>Technologies Used:<span className='font-bold'> React.js, Next.js, Tailwind CSS, GraphQL.</span></p>
@@ -24,12 +24,12 @@ const Projects = () => {
                                 <div className='flex flex-cols-2 items-center justify-center gap-10 p-3 pt-6'>
                                     
                                         <Link href='https://www.tech-ness.com/'>
-                                        <button className='py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300' > 
+                                        <button className='dark:bg-[#395B64] py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300' > 
                                         Demo
                                         </button>
                                         </Link>
                                         
-                                    <button className='py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
                                         <Link href='https://github.com/vsibley/personal-blog-techness'>
                                         Code</Link> 
                                         </button>
@@ -40,19 +40,19 @@ const Projects = () => {
                     </div>
 
                     {/* CARD 2 */}
-                    <div className='shadow-2xl rounded-xl'>
+                    <div className='shadow-2xl rounded-xl dark:shadow-gray-800'>
                         <div className='grid grid-cols-1 gap-4 justify-center items-center text-center'>
                             <Image className='rounded-xl' src={portfolio} alt='' />
                             <div className='p-3'>
                                 <p className='py-2'>Technologies Used:<span className='font-bold'> React.js, Next.js, Tailwind CSS. </span></p>
                                 <p>This is the first portfolio I built with React to showcase the skills I learned and projects I've completed.</p>
                                 <div className='flex flex-cols-2 pt-6 items-center justify-center gap-10 p-3'>
-                                    <button className='py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
                                         <a href="https:/vanessasibley-reactportfolio.netlify.app/">
                                             Demo
                                         </a>
                                     </button>
-                                    <button className='py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300 '>
                                         <a href="https://github.com/vsibley/first-react-portfolio">
 
                                         Code
