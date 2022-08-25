3bca850926e2d386a819efdf5b3b96c24fc78d5b
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-08-25 07:04:09 -0700

    fixed the navbar bg-color on scroll for light UI

--- public/components/Navbar.jsx
+++ public/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     }
 
     return (
-        <div className={shadow ? 'fixed w-full h-20 shadow-md dark:shadow-black dark:bg-[#16161d] dark:text-white z-[100]' : 'fixed w-full h-20 dark:bg dark:text-white z-[100]'}>
+        <div className={shadow ? 'fixed w-full h-20 shadow-md bg-[#ecf0f3] dark:shadow-black dark:bg-[#16161d] dark:text-white z-[100]' : 'fixed w-full h-20  dark:bg dark:text-white z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <a href='/'>
                     <Image src='/assets/logo/logo.png' alt='vs logo' width='85' height='85'/>
