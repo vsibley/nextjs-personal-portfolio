7d083a047cdae21ffa96da8a2398c20ca1eb0f19
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-09-10 10:51:43 -0700

    fixed padding on projects btn for md views and up

--- public/components/Projects.jsx
+++ public/components/Projects.jsx
@@ -14,6 +14,9 @@ const Projects = () => {
                 <h2 className=' py-2 pb-6'>My most recent work</h2>
                 <div className='grid md:grid-cols-3 gap-8 '>
 
+                    {/* CARD ONE  */}
+
+
                     <div className='shadow-2xl rounded-xl dark:shadow-black'>
                         <div className='grid grid-cols-1 gap-4 justify-center items-center text-center p-4 '>
                             <Image className='rounded-xl' src={githubFinder} alt='' />
@@ -23,12 +26,12 @@ const Projects = () => {
                                 <div className='flex flex-cols-2 items-center justify-center gap-10 p-3 pt-6'>
                                     
                                     <a href='https://github-finder-3021w2xvk-vsibley.vercel.app/' target="_blank">
-                                        <button className='bg-[##FF5733] dark:bg-[#395B64] py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300' > 
+                                        <button className='bg-[##FF5733] dark:bg-[#395B64] py-4 px-5 font-bold hover:scale-110 ease-in duration-300' > 
                                         Demo
                                         </button>
                                         </a>
                                         
-                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5 font-bold hover:scale-110 ease-in duration-300'>
                                         <a href='https://github.com/vsibley/github-finder-main' target="_blank">
                                         Code</a> 
                                         </button>
@@ -52,12 +55,12 @@ const Projects = () => {
                                 <div className='flex flex-cols-2 items-center justify-center gap-10 p-3 pt-6'>
                                     
                                     <a href='https://www.tech-ness.com/' target="_blank">
-                                        <button className='bg-[##FF5733] dark:bg-[#395B64] py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300' > 
+                                        <button className='bg-[##FF5733] dark:bg-[#395B64] py-4 px-5 font-bold hover:scale-110 ease-in duration-300' > 
                                         Demo
                                         </button>
                                         </a>
                                         
-                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5  font-bold hover:scale-110 ease-in duration-300'>
                                         <a href='https://github.com/vsibley/personal-blog-techness' target="_blank">
                                         Code</a> 
                                         </button>
@@ -76,12 +79,12 @@ const Projects = () => {
                                 <p className='py-2'>Technologies Used:<span className='font-bold'> React.js, Next.js, Tailwind CSS, Vercel. </span></p>
                                 <p>This is the first portfolio I built with React to showcase the skills I learned and projects I've completed.</p>
                                 <div className='flex flex-cols-2 pt-6 items-center justify-center gap-10 p-3'>
-                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300'>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5 font-bold hover:scale-110 ease-in duration-300'>
                                         <a href="https:/vanessasibley-reactportfolio.netlify.app/" target="_blank">
                                             Demo
                                         </a>
                                     </button>
-                                    <button className='dark:bg-[#395B64]  py-4 px-5 md:px-8 font-bold hover:scale-110 ease-in duration-300 '>
+                                    <button className='dark:bg-[#395B64]  py-4 px-5  font-bold hover:scale-110 ease-in duration-300 '>
                                         <a href="https://github.com/vsibley/first-react-portfolio" target="_blank">
 
                                         Code
